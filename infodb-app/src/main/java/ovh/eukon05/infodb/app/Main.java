da5df93f3461d58aa166d5d6d4bef8b8a619a242
Łukasz Konieczny <lukasz.konieczny.05@outlook.com>
2024-09-04 19:31:28 +0200

    - The test class now runs each source implementation in a separate thread to speed up the test

--- infodb-app/src/main/java/ovh/eukon05/infodb/app/Main.java
+++ infodb-app/src/main/java/ovh/eukon05/infodb/app/Main.java
@@ -2,23 +2,14 @@ package ovh.eukon05.infodb.app;
 
 import ovh.eukon05.infodb.api.ArticleSource;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.ServiceLoader;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         ServiceLoader<ArticleSource> sources = ServiceLoader.load(ArticleSource.class);
 
-        // Test for Onet source's extended article limit
-
         for (ArticleSource source : sources) {
-            if (source.getClass().getName().toLowerCase().contains("onet")) {
-                try (PrintWriter writer = new PrintWriter("out.txt")) {
-                    source.getLatest(210).forEach(a -> writer.println(a.id()));
-                }
-            }
+            new Thread(() -> source.getLatest().forEach(System.out::println)).start();
         }
-
     }
 }
