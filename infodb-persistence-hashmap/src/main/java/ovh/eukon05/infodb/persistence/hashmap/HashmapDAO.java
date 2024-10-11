d9e979f21699771d6a013d41ef472d3ef81e071a
Łukasz Konieczny <lukasz.konieczny.05@outlook.com>
2024-10-11 07:28:07 +0200

    - Cleaned up HashmapDAO and fixed a bug when getLatest would return articles in a reversed order.

--- infodb-persistence-hashmap/src/main/java/ovh/eukon05/infodb/persistence/hashmap/HashmapDAO.java
+++ infodb-persistence-hashmap/src/main/java/ovh/eukon05/infodb/persistence/hashmap/HashmapDAO.java
@@ -5,6 +5,8 @@ import ovh.eukon05.infodb.api.persistence.ArticleDTO;
 import ovh.eukon05.infodb.api.persistence.ArticleSearchCriteria;
 
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class HashmapDAO implements ArticleDAO {
     // I'm reusing the DTO here because it's just a simulation of a real db, and we don't need to create a db entity object
@@ -24,7 +26,7 @@ public class HashmapDAO implements ArticleDAO {
 
     @Override
     public List<ArticleDTO> getLatest(int pageNo) {
-        return repository.values().stream().sorted(Comparator.comparing(ArticleDTO::datePublished)).skip(PAGE_SIZE * pageNo).limit(PAGE_SIZE).toList();
+        return streamToList(repository.values().stream(), pageNo);
     }
 
     @Override
@@ -34,7 +36,16 @@ public class HashmapDAO implements ArticleDAO {
 
     @Override
     public List<ArticleDTO> findByCriteria(ArticleSearchCriteria criteria, int pageNo) {
-        return repository.values().stream().filter(e -> {
+        Stream<ArticleDTO> repoStream = repository.values().stream().filter(getFilter(criteria));
+        return streamToList(repoStream, pageNo);
+    }
+
+    private List<ArticleDTO> streamToList(Stream<ArticleDTO> stream, int pageNo) {
+        return stream.sorted(Comparator.comparing(ArticleDTO::datePublished).reversed()).skip(PAGE_SIZE * pageNo).limit(PAGE_SIZE).toList();
+    }
+
+    private Predicate<ArticleDTO> getFilter(ArticleSearchCriteria criteria) {
+        return e -> {
             if (Optional.ofNullable(criteria.title()).isPresent() && !e.title().toLowerCase().contains(criteria.title().toLowerCase())) {
                 return false;
             }
@@ -48,6 +59,6 @@ public class HashmapDAO implements ArticleDAO {
                 return false;
             }
             return Optional.ofNullable(criteria.dateTo()).isEmpty() || e.datePublished().compareTo(criteria.dateTo()) <= 0;
-        }).sorted(Comparator.comparing(ArticleDTO::datePublished).reversed()).skip(PAGE_SIZE * pageNo).limit(PAGE_SIZE).toList();
+        };
     }
 }
