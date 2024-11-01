34e93953dc8e33b454f5a065ec831fe7b7034eb5
Łukasz Konieczny <lukasz.konieczny.05@outlook.com>
2024-11-01 15:29:49 +0100

    - Enabled CORS for localhost:3000 apps, as I will be trying to create a simple frontend for infodb - Created a test directory for the spring module

--- infodb-app/src/main/java/ovh/eukon05/infodb/app/ApiController.java
+++ infodb-app/src/main/java/ovh/eukon05/infodb/app/ApiController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/articles")
+@CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:3000"})
 class ApiController {
     private final ArticleDAO dao;
 
---
+++ infodb-app/src/test/java/module-info.java
@@ -0,0 +1,2 @@
+module ovh.eukon05.infodb.app.test {
+}
