4998251e458fafe6f4c204fb07c346401554369f
Łukasz Konieczny <lukasz.konieczny.05@outlook.com>
2024-12-18 11:51:04 +0100

    - Fixed application startup through the command line - Updated persistence.xml to the latest version - Fixed a typo in WpSource - Changed fetch delay from 5 to 15 minutes

--- infodb-persistence-hibernate/src/main/java/module-info.java
+++ infodb-persistence-hibernate/src/main/java/module-info.java
@@ -4,7 +4,12 @@ import ovh.eukon05.infodb.persistence.hibernate.HibernateDAO;
 module ovh.eukon05.infodb.persistence.hibernate {
     requires ovh.eukon05.infodb.api.persistence;
     requires jakarta.persistence;
+    requires jakarta.transaction;
     requires org.hibernate.orm.core;
+    requires org.hibernate.commons.annotations;
+    requires com.fasterxml.classmate;
+    requires net.bytebuddy;
+    requires org.jboss.logging;
     provides ArticleDAO with HibernateDAO;
     opens ovh.eukon05.infodb.persistence.hibernate to org.hibernate.orm.core;
 }
--- infodb-source-wp/src/main/java/ovh/eukon05/infodb/source/wp/WpSource.java
+++ infodb-source-wp/src/main/java/ovh/eukon05/infodb/source/wp/WpSource.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class WpSource implements ArticleSource {
-    private static final ArticleSourceInfo sourceInfo = new ArticleSourceInfo("WP", "https://wiadomosci.wp.pl/");
+    private static final ArticleSourceInfo sourceInfo = new ArticleSourceInfo("WP", "https://wiadomosci.wp.pl");
 
     @Override
     public List<Article> getLatest(int limit) {
